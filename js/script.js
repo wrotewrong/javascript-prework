39ce4948fe436417eac38cd1db5a50ff95735650
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2022-07-02 20:37:30 +0200

    Add js code to define computer move

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-let computerMove = `kamień`;
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage(
-  'Zagrałem ' +
-    computerMove +
-    '! Jeśli Twój ruch to ' +
-    playerMove +
-    ', to wygrywasz!'
-);
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+  computerMove = 'kamień';
+} else if (randomNumber == 2) {
+  computerMove = 'papier';
+} else if (randomNumber == 3) {
+  computerMove = 'nożyce';
+}

9e84deb96d3b29ac4cb84c6b3e6ae6e5bb2b8d81
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2022-07-02 19:46:50 +0200

    Add playerMove variable to script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let computerMove = `kamień`;
+let playerMove = 'papier';
+
+printMessage(
+  'Zagrałem ' +
+    computerMove +
+    '! Jeśli Twój ruch to ' +
+    playerMove +
+    ', to wygrywasz!'
+);

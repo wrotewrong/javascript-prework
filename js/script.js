b6a36aa40e56995194a5dc83e2cd9919fda8eed9
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2022-07-03 15:24:52 +0200

    Add js code to define result of the game

--- js/script.js
+++ js/script.js
@@ -27,3 +27,25 @@ if (playerInput == '1') {
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if (computerMove == 'kamień' && playerMove == 'papier') {
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'kamień' && playerMove == 'kamień') {
+  printMessage('Remis!');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+  printMessage('Komputer wygrywa!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'papier') {
+  printMessage('Remis!');
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+  printMessage('Komputer wygrywa!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
+  printMessage('Remis!');
+} else if (computerMove == 'nożyce' && playerMove == 'papier') {
+  printMessage('Komputer wygrywa!');
+} else if (playerMove == 'nieznany ruch') {
+  printMessage('Nieznana komenda! - wpisz cyfrę: 1, 2 lub 3');
+}
